ad68dcb99931c0f42cd6dae061d5ea9358852e4b
ashwiniraykar1997 <ashwiniraykar1997@gmail.com>
2022-03-18 13:41:38 +0530

    projects

---
+++ Iteration/iteration.py
@@ -0,0 +1,20 @@
+def iteration():
+    no = 0
+    while(no < 5):
+        print("Hello")
+        no = (no + 1)
+
+def sequence():
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+
+def main():
+    #sequence()
+    iteration()
+
+
+if __name__ == "__main__":
+    main()
