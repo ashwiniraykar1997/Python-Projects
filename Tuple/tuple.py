5ab6a3694d86831ecc3afc7ef1b31d9cdfe7547b
ashwiniraykar1997 <ashwiniraykar1997@gmail.com>
2022-03-18 14:10:05 +0530

    projects

---
+++ Tuple/tuple.py
@@ -0,0 +1,23 @@
+#Tuple
+# Hetrogenous
+#Indexd
+#Sequential
+#ordered
+#Allow duplicate
+#Data  of Tuple is Immutable
+# Tuple is immutable
+
+
+
+
+
+data = (10,20,30,40,"Marvellous",10)
+
+print("Data Type is:",type(data))
+print("Elemnt are:",data)
+print("0th element is:",data[0])
+print("3rd index elemnt:",data[3])
+print("Last Element:",data[-1])
+# data[0] = 11
+# data.append(50)
+# print("Updated Data:",data)
