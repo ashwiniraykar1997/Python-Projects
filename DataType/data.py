b5e3f6fbf9a5f2c1f458423a07d7972b733c594f
ashwiniraykar1997 <ashwiniraykar1997@gmail.com>
2022-03-18 13:34:22 +0530

    projects

---
+++ DataType/data.py
@@ -0,0 +1,41 @@
+no = 11
+print(type(no))
+print(no)
+
+value = 3.14
+print(type(value))
+print(value)
+
+
+x = True
+print(type(x))
+print(x)
+
+
+data = 10 + 5j;
+print(type(data))
+print(data)
+
+
+i = None
+print(type(i))
+print(i)
+
+value = 11
+print(value)
+print(type(value))
+
+value = None
+print(value)
+print(type(value))
+
+value =3.14
+print(value)
+print(type(value))
+
+demo = None
+print(type(None))
+demo = [121,12]
+
+
+
---
+++ DataType/datatype.py
@@ -0,0 +1,5 @@
+
+no1 =int(input("Enter First number:"))
+no2 = int(input("Enter First number:"))
+ans = no1 + no2
+print("Addition is",ans)
